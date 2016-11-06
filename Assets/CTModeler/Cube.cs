b2642b1cc78fa3e99f7c1267906e7733223de60e
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-05 20:08:32 -0400

    We can create cubes now

---
+++ Assets/CTModeler/Cube.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    
+    public class Cube : Shape {
+        protected override Mesh CreateMesh() {
+            Vector3[] vertices = new Vector3[6 * 4];
+            Vector4[] tangents = new Vector4[6 * 4];
+            Vector3[] normals = new Vector3[6 * 4];
+            Vector2[] uvs = new Vector2[6 * 4];
+            int[] triangles = new int[6 * 2 * 3];
+
+            Vector3[,] faces = new Vector3[6, 3] {
+                { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) },
+                { new Vector3(0, 1, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0) },
+                { new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(0, 1, 0) },
+                { new Vector3(0, 0, -1), new Vector3(-1, 0, 0), new Vector3(0, 1, 0) },
+                { new Vector3(0, -1, 0), new Vector3(0, 0, -1), new Vector3(1, 0, 0) },
+                { new Vector3(-1, 0, 0), new Vector3(0, -1, 0), new Vector3(0, 0, 1) },
+            };
+
+            Mesh mesh = new Mesh();
+
+            for(int i = 0; i < 6; i++) {
+                vertices[i * 4 + 0] = faces[i, 0] - faces[i, 1] - faces[i, 2];
+                vertices[i * 4 + 1] = faces[i, 0] + faces[i, 1] - faces[i, 2];
+                vertices[i * 4 + 2] = faces[i, 0] + faces[i, 1] + faces[i, 2];
+                vertices[i * 4 + 3] = faces[i, 0] - faces[i, 1] + faces[i, 2];
+
+                triangles[i * 6 + 0] = i * 4 + 0;
+                triangles[i * 6 + 1] = i * 4 + 1;
+                triangles[i * 6 + 2] = i * 4 + 2;
+
+                triangles[i * 6 + 3] = i * 4 + 3;
+                triangles[i * 6 + 4] = i * 4 + 0;
+                triangles[i * 6 + 5] = i * 4 + 2;
+
+                tangents[i * 4 + 0] = faces[i, 1];
+                tangents[i * 4 + 1] = faces[i, 1];
+                tangents[i * 4 + 2] = faces[i, 1];
+                tangents[i * 4 + 3] = faces[i, 1];
+
+                normals[i * 4 + 0] = faces[i, 0];
+                normals[i * 4 + 1] = faces[i, 0];
+                normals[i * 4 + 2] = faces[i, 0];
+                normals[i * 4 + 3] = faces[i, 0];
+
+                uvs[i * 4 + 0] = new Vector2(0, 0);
+                uvs[i * 4 + 1] = new Vector2(1, 0);
+                uvs[i * 4 + 2] = new Vector2(1, 1);
+                uvs[i * 4 + 3] = new Vector2(0, 1);
+            }
+
+            mesh.vertices = vertices;
+            mesh.tangents = tangents;
+            mesh.normals = normals;
+            mesh.uv = uvs;
+            mesh.triangles = triangles;
+
+            return mesh;
+        }
+    }
+}
---
+++ Assets/CTModeler/Shape.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace CT {
+    
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public abstract class Shape : MonoBehaviour {
+
+        // Use this for initialization
+        public virtual void Start() {
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = CreateMesh();
+        }
+	
+        // Update is called once per frame
+        public virtual void Update() {
+        }
+
+        protected abstract Mesh CreateMesh();
+
+        // TODO: is this correct?
+        protected int[] GenerateTriangleStripIndices(int numTris) {
+            int[] tris = new int[numTris*3];
+            for (int i = 0; i < numTris; i++) {
+                if (i % 2 == 0) {
+                    tris[i*3] = i;
+                    tris[i*3+1] = i+1;
+                    tris[i*3+2] = i+2;
+                }
+                else {
+                    tris[i*3] = i+1;
+                    tris[i*3+1] = i;
+                    tris[i*3+2] = i+2;
+                }
+            }
+            return tris;
+        }
+    }
+}
