b42ad9d417080dd19464783ad5808c9aa1c253e0
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-08 18:14:08 -0500

    Added an open cylinder

---
+++ Assets/CTModeler/OpenCylinder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    public class OpenCylinder : Revolved {
+        protected override Vector2 RevolutionFunction(float t) {
+            return new Vector2(1, 2 * t - 1);
+        }
+    }
+}
