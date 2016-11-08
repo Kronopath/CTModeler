a74efb348bbbbdd89f2e4771ad0336fbf3024e58
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-08 18:28:17 -0500

    Added a Disk class

---
+++ Assets/CTModeler/Disk.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    public class Disk : Revolved {
+
+        // TODO: write custom editor that only shows num segments and hides numU and numV
+        public int numSegments = 10;
+
+        public override void Start() {
+            numU = numSegments;
+            numV = 1;
+            base.Start();
+        }
+
+        protected override Vector2 RevolutionFunction(float t) {
+            return new Vector2(t + 0.001f, 0);
+        }
+    }
+}
