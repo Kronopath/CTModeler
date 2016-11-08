acf9d4fb5a35795b9fa2df23a5aee0eea1de547f
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-08 17:59:05 -0500

    Added support for extruded objects

---
+++ Assets/CTModeler/Extruded.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    public abstract class Extruded : Parametric {
+        
+        protected abstract Vector2 ExtrusionProfileFunction(float u, float v);
+
+        protected abstract Vector3 ExtrusionPathFunction(float v);
+
+        protected override Vector3 ParametricFunction(float u, float v) {
+            Vector2 xy = ExtrusionProfileFunction(u, v);
+            Vector3 path = ExtrusionPathFunction(v);
+            Vector3 path2 = ExtrusionPathFunction(v + 0.001f);
+            Vector3 dz = (path2 - path).normalized;
+            Vector3 dx = Vector3.right;
+            Vector3 dy = Vector3.Cross(dx, dz).normalized;
+            return new Vector3(path.x + xy.x * dx.x + xy.y * dy.x,
+                               path.y + xy.x * dx.y + xy.y * dy.y,
+                               path.z + xy.x * dx.z + xy.y * dy.z);
+        }
+    }
+}
---
+++ Assets/CTModeler/Knot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    public class Knot : Extruded {
+
+        private const float TAU = 2 * Mathf.PI;
+
+        protected override Vector3 ExtrusionPathFunction(float v) {
+            v *= 2 * TAU;
+            float r = 1 + Mathf.Cos(1.5f * v) / 5;
+            return new Vector3(Mathf.Sin(1.5f * v) / 2,
+                               r * Mathf.Cos(v),
+                               r * Mathf.Sin(v));
+        }
+
+        protected override Vector2 ExtrusionProfileFunction(float u, float v) {
+            v = 0.15f + 0.06f * Mathf.Cos(3 * TAU * v);
+            u *= TAU;
+            return new Vector2(v*Mathf.Cos(u), v*Mathf.Sin(u));
+        }
+    }
+}
