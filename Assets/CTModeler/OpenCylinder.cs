5386bcfaa8f3ebc81913ad8d1f26ce8e4db205d3
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-08 18:28:06 -0500

    Allow the user to specify the number of segments in a cylinder.
    Currently numU and numV are still shown for the cylinder, but are technically unnecessary. We’ll later get rid of them in the editor.

--- Assets/CTModeler/OpenCylinder.cs
+++ Assets/CTModeler/OpenCylinder.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 namespace CT {
     public class OpenCylinder : Revolved {
+
+        // TODO: write custom editor that only shows num segments and hides numU and numV
+        public int numSegments = 10;
+
+        public override void Start() {
+            numU = numSegments;
+            numV = 1;
+            base.Start();
+        }
+
         protected override Vector2 RevolutionFunction(float t) {
             return new Vector2(1, 2 * t - 1);
         }
