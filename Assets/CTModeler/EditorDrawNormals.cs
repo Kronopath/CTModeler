6a55ab58b732e929f7ccae83ce7256ef7c4b39b8
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-21 03:41:12 -0500

    Introduced a new utility class to draw the normals of the shapes

---
+++ Assets/CTModeler/EditorDrawNormals.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CT {
+    /// <summary>
+    /// Utility class that draws normals for an object in the editor when selected.
+    /// Meant for debugging.
+    /// </summary>
+    public class EditorDrawNormals : MonoBehaviour {
+        private MeshFilter meshFilter;
+
+        public void Start() {
+            meshFilter = GetComponent<MeshFilter>();
+    }
+
+        public void OnDrawGizmosSelected() {
+            Gizmos.color = Color.red;
+            if(meshFilter && meshFilter.mesh) {
+                for(int i = 0; i < meshFilter.mesh.vertices.Length; i++) {
+                    Vector3 vertexPos = transform.TransformPoint(meshFilter.mesh.vertices[i]);
+                    Gizmos.DrawLine(vertexPos,
+                                vertexPos + meshFilter.mesh.normals[i]);
+                }
+            }
+    }
+    }
+}
