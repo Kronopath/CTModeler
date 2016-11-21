c19eb6370c989ac8d92ec7dbe327bc48bc790cbb
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-21 16:34:58 -0500

    Beginning some work on some spline utilities

---
+++ Assets/CTModeler/Shapes/Cup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CT {
+    public class Cup : Revolved {
+        private List<Vector3> spline;
+
+        public override void Start() {
+            spline = SplineUtils.MakeSpline(
+                new List<Vector3>{
+                    new Vector3(1, 0.2f), new Vector3(0.2f, 1)
+                }
+            );
+            base.Start();
+        }
+
+        protected override Vector2 RevolutionFunction(float t) {
+            return SplineUtils.Sample(spline, t);
+        }
+    }
+}
---
+++ Assets/CTModeler/Utilities/SplineUtils.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CT {
+    public static class SplineUtils {
+        public static List<Vector3> MakeSpline(List<Vector3> keys) {
+            List<Vector3> spline = new List<Vector3>();
+
+            int segmentsPerKey = 10;
+            if(keys.Count < 2) {
+                return new List<Vector3>(keys);
+            }
+            else if(keys.Count == 2) {
+                for(int i = 0; i <= segmentsPerKey; i++) {
+                    spline.Add(Vector3.Lerp(keys[0], keys[1], (float)i / segmentsPerKey));
+                }
+            }
+
+            // TODO: actual splining
+            return spline;
+        }
+
+        // TODO DOC THIS
+        // t should be between 0 and 1!
+        public static Vector3 Sample(List<Vector3> points, float t) {
+            t = Mathf.Clamp(t, 0, 0.999f);
+            if(points.Count < 1) {
+                return Vector3.zero;
+            }
+            else if(points.Count == 1) {
+                return points[0];
+            }
+
+            int intPart = Mathf.FloorToInt((points.Count - 1) * t);
+            float fractPart = (points.Count - 1) * t - intPart;
+            return Vector3.Lerp(points[intPart], points[intPart + 1], fractPart);
+        }
+    }
+}
