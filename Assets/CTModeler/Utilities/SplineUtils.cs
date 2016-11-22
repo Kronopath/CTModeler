e5ddf35f69dbb0b9cc2aa9cb7f6c6515657abfe6
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-21 19:32:24 -0500

    Continued implementing spline utilities and a demo object that uses them

--- Assets/CTModeler/Utilities/SplineUtils.cs
+++ Assets/CTModeler/Utilities/SplineUtils.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace CT {
     public static class SplineUtils {
+        // Unity's version of Mono doesn't accept more than 4 arguments in a Func<T...>, sadly, so
+        // this is necessary.
+        private delegate Vector3 HermiteFunc (Vector3 a,Vector3 da,Vector3 b,Vector3 db,float t);
+
         public static List<Vector3> MakeSpline(List<Vector3> keys) {
             List<Vector3> spline = new List<Vector3>();
 
@@ -17,7 +22,51 @@ namespace CT {
                 }
             }
 
-            // TODO: actual splining
+            HermiteFunc hermite =
+                (Vector3 a, Vector3 da, Vector3 b, Vector3 db, float t) => {
+                    float tt = t * t;
+                    float ttt = tt * t;
+                    return a * (2 * ttt - 3 * tt + 1)
+                    + da * (ttt - 2 * tt + t)
+                    + b * (-2 * ttt + 3 * tt)
+                    + db * (ttt - tt);
+                };
+
+            List<float> lengths = new List<float>();
+            for(int n = 0; n < keys.Count - 1; n++) {
+                lengths.Add((keys[n + 1] - keys[n]).magnitude);
+            }
+
+            List<Vector3> D = new List<Vector3>();
+            for(int n = 0; n < keys.Count; n++) {
+                if(n == 0) {
+                    D.Add((3 * keys[n + 1] - 2 * keys[n] - keys[n + 2]) / 3);
+                }
+                else if(n < keys.Count - 1) {
+                    D.Add(
+                        (lengths[n] * (keys[n] - keys[n - 1])
+                        + lengths[n - 1] * (keys[n + 1] - keys[n]))
+                        / (lengths[n - 1] + lengths[n]));
+                }
+                else {
+                    D.Add((2 * keys[n] - 3 * keys[n - 1] + keys[n - 2]) / 3);
+                }
+            }
+
+            if(keys[0] == keys[keys.Count - 1]) {
+                Vector3 average = (D[0] + D[keys.Count - 1]) / 2;
+                D[0] = average;
+                D[keys.Count - 1] = average;
+            }
+
+            for(int n = 0; n < keys.Count - 1; n++) {
+                for(int i = 0; i < segmentsPerKey; i++) {
+                    spline.Add(hermite(keys[n], D[n] * 0.9f, keys[n + 1], D[n + 1] * 0.9f,
+                                       i / segmentsPerKey));
+                }
+            }
+            spline.Add(keys[keys.Count - 1]);
+
             return spline;
         }
 
