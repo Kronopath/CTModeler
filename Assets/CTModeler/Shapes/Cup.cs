d4caa2c416c8b7b626a67d97f65b5bfa12f214e0
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-22 15:45:29 -0500

    Nicer formatting in Cup object

--- Assets/CTModeler/Shapes/Cup.cs
+++ Assets/CTModeler/Shapes/Cup.cs
@@ -8,12 +8,23 @@ namespace CT {
 
         public override void Start() {
             spline = SplineUtils.MakeSpline(
-                new List<Vector3> {new Vector3(.001f, -1f), new Vector3(.5f, -1f),
-                    new Vector3(.45f, -.95f), new Vector3(.15f, -.9f), new Vector3(.09f, -.85f),
-                    new Vector3(.05f, -.6f), new Vector3(.1f, 0f), new Vector3(.2f, .1f),
-                    new Vector3(.4f, .2f), new Vector3(.6f, .5f), new Vector3(.5f, .98f),
-                    new Vector3(.48f, 1f), new Vector3(.46f, .98f), new Vector3(.55f, .5f),
-                    new Vector3(.35f, .3f), new Vector3(.001f, .2f)
+                new List<Vector3> {
+                    new Vector3(.001f, -1f),
+                    new Vector3(.5f, -1f),
+                    new Vector3(.45f, -.95f),
+                    new Vector3(.15f, -.9f),
+                    new Vector3(.09f, -.85f),
+                    new Vector3(.05f, -.6f),
+                    new Vector3(.1f, 0f),
+                    new Vector3(.2f, .1f),
+                    new Vector3(.4f, .2f),
+                    new Vector3(.6f, .5f),
+                    new Vector3(.5f, .98f),
+                    new Vector3(.48f, 1f),
+                    new Vector3(.46f, .98f),
+                    new Vector3(.55f, .5f),
+                    new Vector3(.35f, .3f),
+                    new Vector3(.001f, .2f)
                 }
             );
             base.Start();
