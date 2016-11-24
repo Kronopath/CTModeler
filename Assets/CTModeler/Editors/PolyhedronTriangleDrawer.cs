9030ca52cec935ee2b9994caff0e5704e2231ea3
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-24 17:16:26 -0500

    Started work on custom drawer for polyhedrons’ triangles

---
+++ Assets/CTModeler/Editors/PolyhedronTriangleDrawer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace CT {
+    [CustomPropertyDrawer(typeof(Polyhedron.Triangle))]
+    public class PolyhedronTriangleDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            using (var pScope = new EditorGUI.PropertyScope(position, label, property)) {
+                float width = position.width/3f;
+
+                EditorGUI.PropertyField(new Rect(position.x, position.y, width, position.height),
+                                        property.FindPropertyRelative("v1"),
+                                        GUIContent.none);
+                
+                EditorGUI.PropertyField(new Rect(position.x+width, position.y,
+                                                 width, position.height),
+                                        property.FindPropertyRelative("v2"),
+                                        GUIContent.none);
+
+                EditorGUI.PropertyField(new Rect(position.x+width*2, position.y,
+                                                 width, position.height),
+                                        property.FindPropertyRelative("v3"),
+                                        GUIContent.none);
+            }
+        }
+    }
+}
