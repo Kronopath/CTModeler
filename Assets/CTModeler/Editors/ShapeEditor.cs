eaf12546dc9ce968bf35a16cb77a82416c2c094c
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-24 16:35:15 -0500

    Ensure that shapes are updated and applied as needed, just to be safe

--- Assets/CTModeler/Editors/ShapeEditor.cs
+++ Assets/CTModeler/Editors/ShapeEditor.cs
@@ -7,12 +7,14 @@ namespace CT {
     [CanEditMultipleObjects]
     public class ShapeEditor : Editor {
         public override void OnInspectorGUI() {
+            serializedObject.Update();
             DrawDefaultInspector();
 
             Shape s = (Shape)target;
             if(GUILayout.Button("Regenerate Mesh")) {
                 s.RegenerateMesh();
             }
+            serializedObject.ApplyModifiedProperties();
         }
     }
 
