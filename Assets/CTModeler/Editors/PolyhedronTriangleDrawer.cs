ada6f1ff4de9c726ebcbd585c3f85933f132ecef
Gabriel B. Nunes <gabriel@kronopath.net>
2016-11-24 17:44:11 -0500

    Better way of drawing the Polyhedron tris, with labels

--- Assets/CTModeler/Editors/PolyhedronTriangleDrawer.cs
+++ Assets/CTModeler/Editors/PolyhedronTriangleDrawer.cs
@@ -6,23 +6,41 @@ namespace CT {
     [CustomPropertyDrawer(typeof(Polyhedron.Triangle))]
     public class PolyhedronTriangleDrawer : PropertyDrawer {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            using (var pScope = new EditorGUI.PropertyScope(position, label, property)) {
-                float width = position.width/3f;
-
-                EditorGUI.PropertyField(new Rect(position.x, position.y, width, position.height),
-                                        property.FindPropertyRelative("v1"),
-                                        GUIContent.none);
+            using(var pScope = new EditorGUI.PropertyScope(position, label, property)) {
                 
-                EditorGUI.PropertyField(new Rect(position.x+width, position.y,
-                                                 width, position.height),
-                                        property.FindPropertyRelative("v2"),
-                                        GUIContent.none);
+                position = EditorGUI.IndentedRect(position);
+                int indentLevel = EditorGUI.indentLevel;
+                EditorGUI.indentLevel = 0;
 
-                EditorGUI.PropertyField(new Rect(position.x+width*2, position.y,
-                                                 width, position.height),
-                                        property.FindPropertyRelative("v3"),
-                                        GUIContent.none);
+                float width = position.width / 3f;
+                float labelWidth = 30f;
+
+                FieldWithLabel(new Rect(position.x, position.y, width, position.height),
+                               "V1", labelWidth, property.FindPropertyRelative("v1"));
+
+                FieldWithLabel(new Rect(position.x + width, position.y, width, position.height),
+                               "V2", labelWidth, property.FindPropertyRelative("v2"));
+                
+                FieldWithLabel(new Rect(position.x + width * 2, position.y, width, position.height),
+                               "V3", labelWidth, property.FindPropertyRelative("v3"));
+
+                EditorGUI.indentLevel = indentLevel;
             }
+        }
+
+        private void FieldWithLabel(Rect position, string label, float labelWidth,
+                                    SerializedProperty property)
+        {
+            EditorGUI.HandlePrefixLabel(
+                position,
+                new Rect(position.x, position.y, labelWidth, position.height),
+                new GUIContent(label));
+            
+            EditorGUI.PropertyField(
+                new Rect(position.x + labelWidth, position.y,
+                         position.width - labelWidth - 5, position.height),
+                property,
+                GUIContent.none);
         }
     }
 }
